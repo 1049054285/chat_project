40623e727b74d71a9eac527587a516ebc9c41715
linzetong <1049054285@qq.com>
2018-09-12 18:11:29 +0800

    11

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,6 +4,8 @@ import json
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        self.user = self.scope["user"]
+        print(self.user)
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
